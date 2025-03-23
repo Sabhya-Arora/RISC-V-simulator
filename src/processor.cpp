b6e01e7cd27a3154fb93d37ea6191f13d5e5cbed
Sabhya-Arora <sabhyaarora2004@gmail.com>
2025-03-23 14:42:24 +0530

    Added functions for id and ex

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,16 +1,148 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <bits/stdc++.h>
+#include "processor.hpp"
 using namespace std;
+vector<string> program;
+vector<string> assembly;
+vector<int> regs(32, 0);
 
+
+string IF(int pc) {
+    return program[pc];
+}
+
+struct ID_EX ID(struct IF_ID inst) {
+    struct ID_EX id_ex;
+    string str = inst.instruction;
+    int rs1 = stoi(str.substr(12, 5), nullptr, 2);
+    int rs2 = stoi(str.substr(7, 5), nullptr, 2);
+    int rd = stoi(str.substr(20, 5), nullptr, 2);
+    int funct3 = stoi(str.substr(17, 3), nullptr, 2);
+    int funct7 = stoi(str.substr(0, 7), nullptr, 2);
+    int opcode = stoi(str.substr(25, 7), nullptr, 2);
+    int imm = 0;
+    id_ex.rs1 = rs1;
+    id_ex.rs2 = rs2;
+    id_ex.rd = rd;
+    id_ex.pc = inst.pc;
+    switch (opcode) {
+        case 51: //R-type
+            if (funct3 == 0) {
+                if (funct7 == 0) {
+                    id_ex.alu_op = 0;
+                } else if (funct7 == 32) {
+                    id_ex.alu_op = 1;
+                }
+            }
+            break;
+        case 3: //I-type
+            if (funct3 == 2) { // lw
+                imm = stoi(str.substr(0, 12), nullptr, 2);
+                id_ex.alu_op = 2;
+            }
+            break;
+        case 19: //I-type
+            if (funct3 == 0) { // addi
+                imm = stoi(str.substr(0, 12), nullptr, 2);
+                id_ex.alu_op = 3;
+            }
+            break;
+        case 35: //S-type
+            if (funct3 == 2) { // sw, rs1 is address
+                imm = stoi(str.substr(0, 7) + str.substr(20, 5), nullptr, 2);
+                id_ex.alu_op = 4;
+            }
+            break;
+        case 111: //rd, imm
+            imm = stoi(str.substr(0, 20), nullptr, 2);
+            id_ex.alu_op = 5;
+            break;
+    }
+    
+    return id_ex;
+}
+
+void EX() {
+
+}
+
+void MEM() {
+
+}
+
+void WB() {
+
+}
+
+string hexToBinary(string hex) {
+    string binary = "";
+    for (char c : hex) {
+        switch (toupper(c)) {
+            case '0': binary += "0000"; break;
+            case '1': binary += "0001"; break;
+            case '2': binary += "0010"; break;
+            case '3': binary += "0011"; break;
+            case '4': binary += "0100"; break;
+            case '5': binary += "0101"; break;
+            case '6': binary += "0110"; break;
+            case '7': binary += "0111"; break;
+            case '8': binary += "1000"; break;
+            case '9': binary += "1001"; break;
+            case 'A': binary += "1010"; break;
+            case 'B': binary += "1011"; break;
+            case 'C': binary += "1100"; break;
+            case 'D': binary += "1101"; break;
+            case 'E': binary += "1110"; break;
+            case 'F': binary += "1111"; break;
+        }
+    }
+    return binary;
+}
 int main(int argc, char * argv[]) {
     char *file;
     file = argv[1];
+    int clk_cycle = atoi(argv[2]);
     FILE *fp = fopen(file, "r");
-    char inp[8];
+    char inp[100];
     while (fgets(inp, sizeof(inp), fp)) {
-        
+        int ind = 0;
+        string mach_code = "";
+        string assembly_code = "";
+        while (inp[ind] != ':') {
+            ind++;
+        }
+        ind++;
+        while (inp[ind] == ' ') {
+            ind++;
+        }
+        while (inp[ind] != ' ') {
+            mach_code += inp[ind];
+            ind++;
+        }
+        while (inp[ind] == ' ') {
+            ind++;
+        }
+        while (inp[ind] != '\n') {
+            assembly_code += inp[ind];
+            ind++;
+        }
+        string str = hexToBinary(mach_code);
+        program.push_back(str);
+        assembly.push_back(assembly_code);
     }
+    int pc = 0;
+    for (int i = 0; i < clk_cycle; i++) {
+        //IF
+        struct IF_ID l2 = {pc, IF(pc)};
+        //ID
+
+        //EX
+
+        //MEM
+
+        //WB
+
+    }
+    
+    
     return 0;
 }
